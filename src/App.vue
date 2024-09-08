1ada9e41e4d444cba8c99698d88658ac2bb18e82
torry2 <mail@torry.to>
2024-09-08 14:00:27 +0800

    components

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="quiz">
+    <Question :questionText="question" />
+    <Answers :options="answerOptions" />    
+  </div>  
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import Question from './components/question.vue';
+import Answers from './components/answers.vue';
+import Result from './components/result.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  components: {
+    Question,
+    Answers,
+    Result
+  },
+  data() {
+    return {
+      question: 'This is a Tax Related Question',
+      answerOptions: ['Answer A', 'Answer B', 'Answer C', 'Answer D', 'Answer E']
+    };
   }
+};
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+
+
 </style>
---
+++ src/components/answers.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="answers-box">
+    <button v-for="(option, index) in options" :key="index" class="answer-button" @click="selectAnswer(option)">
+      {{ option }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    selectAnswer(option) {
+      alert(option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.answers-box {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.answer-button {
+  padding: 10px;
+  background-color: #ffffff;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.answer-button:hover {
+  background-color: ##ababab;
+}
+</style>
